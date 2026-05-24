5cbfc88eeb3777f1272a08914d8643f2b228cb12
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-05-24 15:13:31 +0200

    buffor.from check

--- src/shared/hooks/useSolanaDevnet.ts
+++ src/shared/hooks/useSolanaDevnet.ts
@@ -70,7 +70,7 @@ export function useSolanaDevnet(certId: string, certHash: string) {
       const instruction = new TransactionInstruction({
         keys:      [{ pubkey: publicKey, isSigner: true, isWritable: false }],
         programId: MEMO_PROGRAM_ID,
-        data:      new TextEncoder().encode(memoPayload),
+        data:      Buffer.from(memoPayload, "utf8"),
       });
 
       const latestBlockhash = await connection.getLatestBlockhash();
